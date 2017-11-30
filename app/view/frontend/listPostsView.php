704ca467725844f262f3c1dac3a42cefe7c91e61
PICTA\picta <pictapica@gmail.com>
2017-11-30 15:39:02 +0100

    MODIF view & css

--- app/view/frontend/listPostsView.php
+++ app/view/frontend/listPostsView.php
@@ -1,32 +1,39 @@
+
 <?php $title = 'Mon blog'; ?>
 
 <?php ob_start(); ?>
-<h1>Mon super blog !</h1>
-<p>Derniers billets du blog :</p>
+<div class="container">
+
+    <header class="row">
+        <div class="col-lg-12">
+            <h1>Mon super blog !</h1>
+        </div>
+    </header>
+    
+    <p>Derniers billets du blog :</p>
 
 
-<?php
-while ($data = $posts->fetch())
-{
-?>
-<div id="contener">
-    <div class="news">
-        <h3>
-            <?= htmlspecialchars($data['title']) ?>
-            <em>le <?= $data['creation_date_fr'] ?></em>
-        </h3>
-        
-        <p>
-            <?= nl2br(htmlspecialchars($data['content'])) ?>
-            <br /><br /><br />
-            <em><a href="index.php?action=post&amp;id=<?= $data['id'] ?>">Commentaires</a></em>
-        </p>
-    </div>
-</div>
-<?php
-}
-$posts->closeCursor();
-?>
-<?php $content = ob_get_clean(); ?>
+    <?php
+    while ($data = $posts->fetch()) {
+        ?>
+        <div id="contener">
+            <div class="news">
+                <h3>
+                    <?= htmlspecialchars($data['title']) ?>
+                    <em>le <?= $data['creation_date_fr'] ?></em>
+                </h3>
 
-<?php require('template.php'); ?>
+                <p>
+                    <?= nl2br(htmlspecialchars($data['content'])) ?>
+                    <br /><br /><br />
+                    <em><a href="index.php?action=post&amp;id=<?= $data['id'] ?>">Commentaires</a></em>
+                </p>
+            </div>
+        </div>
+        <?php
+    }
+    $posts->closeCursor();
+    ?>
+    <?php $content = ob_get_clean(); ?>
+
+    <?php require('template.php'); ?>
--- app/view/frontend/postView.php
+++ app/view/frontend/postView.php
@@ -1,43 +1,45 @@
 <?php $title = htmlspecialchars($post['title']); ?>
 
 <?php ob_start(); ?>
-<h1>Mon super blog !</h1>
-<p><a href="index.php">Retour à la liste des billets</a></p>
+
+    <h1>Mon super blog !</h1>
+    <p><a href="index.php">Retour à la liste des billets</a></p>
 
     <div class="post">
         <h3>
             <?= htmlspecialchars($post['title']) ?>
             <em>le <?= $post['creation_date_fr'] ?></em>
         </h3>
-        
+
         <p>
             <?= nl2br(htmlspecialchars($post['content'])) ?>
         </p>
     </div>
-    
+
     <div class="comments">
         <h2>Commentaires</h2>
-            <form action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
-                <div>
-                    <label for="author">Auteur</label><br />
-                    <input type="text" id="author" name="author" />
-                </div>
-                <div>
-                    <label for="comment">Commentaire</label><br />
-                    <textarea id="comment" name="comment"></textarea>
-                </div>
-                <div>
-                    <input type="submit" />
-            </form>
-    </div><br />
+        <form action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
+            <div>
+                <label for="author">Auteur</label><br />
+                <input type="text" id="author" name="author" />
+            </div>
+            <div>
+                <label for="comment">Commentaire</label><br />
+                <textarea id="comment" name="comment"></textarea>
+            </div>
+            <div>
+                <input type="submit" />
+            </div>
+        </form>
+    </div>
+><br />
 <?php
-while ($comment = $comments->fetch())
-{
-?>
+while ($comment = $comments->fetch()) {
+    ?>
     <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['comment_date_fr'] ?></p>
     <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p><br />
 
-<?php
+    <?php
 }
 ?>
 <?php $content = ob_get_clean(); ?>
