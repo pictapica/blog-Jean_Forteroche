6dde3a50970769eebdbb69fa3eab00c8ea7f4394
PICTA\picta <pictapica@gmail.com>
2017-12-10 09:07:27 +0100

    MODIF controler/login.php & model/UserManager

--- app/model/UserManager.php
+++ app/model/UserManager.php
@@ -4,20 +4,13 @@ require_once ("Manager.php");//Pour se connecter à la base de données
 
 class UserManager extends Manager
 {
-    /**
-     * 
-     * @param type $pseudo
-     * @param type $password
-     * @return type
-     * 
-     */ 
+    
     public function connect ($pseudo, $password) {
         $db = $this->dbConnect();
         $req = $db->prepare('SELECT * FROM user WHERE pseudo = :pseudo AND password = :password');
         $req->execute(array(
             'pseudo' => $pseudo,
-            'password' => hash ("sha1", $password),  
-        ));
+            'password' => $pass_hache));
         return $result = $req->fetch();
         
     }
